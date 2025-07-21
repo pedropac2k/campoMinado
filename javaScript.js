811d88b1b919a37cef78937399067e2f0158ced1
pedropac2k <134807106+pedropac2k@users.noreply.github.com>
2025-07-21 18:40:27 -0300

    Função criaMatriz adicionada. Foram adicionados também os comentários ao código.

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,58 @@
+import * as math from "mathjs";
+
+
 class Square {
+	//Toda vez que uma instância for criada ela será inicializada com estes atributos por padrão.
+	//É importante notar que os atributos 'row' e 'column' serão únicos para cada instância pois indentificam o posicionamento dela na matriz.
 	constructor(row, column){
 		this.row = row;
 		this.column = column;
 		this.state = "closed";
-		this.hasMine = "false";
+		this.hasMine = false;
 		this.nearMines = 0;
 	}
 }
 
-function criaMatriz(row, column){
-	let matriz = [];
 
-	for(let i=0; i<row; i++){
+
+function criaMatriz(rows, columns){
+	let matriz = [];
+	//É necessário declarar um vetor antes de atribuir outros vetores a ele para criar uma matriz.
+
+	for(let i=0; i<rows; i++){
 		matriz[i] = [];
-		for(let j=0; j<column; j++){
+		//Declara 'rows' vetores, cada um deles contendo outro vetor.
+		for(let j=0; j<columns; j++){
 			matriz[i][j] = new Square(i, j);
+			//Determina a quantidade 'columns' de colunas que cada linha terá, e atribui uma instância a cada posição,
+			//inicializando sua coordenada na matriz. 
 		}
 	}
 
 	return matriz;
+}
+
+
+function sorteiaMinas(matriz, qtdMinas){
+	let minasAtivadas = 0;
+
+	const rows = matriz.length;
+	const columns = matriz[0].length;
+	//Extrai as dimensões da matriz, sem precisarem ser passadas por parâmetro.
+
+	while(minasAtivadas != qtdMinas){
+		//Bloco é repetido até atingirmos o número de minas ativadas.
+		let rowSorteada = math.floor(math.random() * rows );
+		let columnSorteada = math.floor(math.random() * columns);
+		//Sorteia indices através da função math.random(), que gera um número aleatório entre 0 e 1(exclusivo)
+		//A função math.floor é usada para arredondar o número para o inteiro mais baixo, já que estamos lidando com
+		//índices não pode ser ponto flutuante e não podemos obter o valor do tamanho do array.
+
+		if (!matriz[rowSorteada][columnSorteada].hasMine){
+			//Se esta coordenada não possuí uma mina ativa executa o bloco.
+			matriz[rowSorteada][columnSorteada].hasMine = true;
+			minasAtivadas++;
+			//Ativa a mina e adiciona ao contador para termos o controle de quantas minas vão ser ativadas.
+		}
+	}
 }
